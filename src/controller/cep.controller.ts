95a02cca5ef7bb0eec17d7f59a10642e00ed3772
rafapearl <rafaironm2@gmail.com>
2022-01-17 18:00:45 -0300

    ...

--- src/controller/cep.controller.ts
+++ src/controller/cep.controller.ts
@@ -13,3 +13,19 @@ export const postCeps = async (req: Request, res: Response) => {
     })
 }
 
+export const buscaCep = async (req: Request, res: Response) => {
+    const { cep } = req.body
+
+}
+
+(async () => {
+  const browser = await puppeteer.launch( { headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://www2.correios.com.br/sistemas/buscacep/buscaEndereco.cfm');
+
+  await page.type('[id="cep"]', " 07051000")
+
+  await page.click('[type="submit"]')
+
+  // await browser.close();
+})(); 
